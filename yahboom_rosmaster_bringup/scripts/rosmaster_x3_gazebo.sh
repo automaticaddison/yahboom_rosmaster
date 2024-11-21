14d9b58ca12446edb66472ac1efa5da5cb8d08f7
Automatic Addison <automaticaddison@gmail.com>
2024-11-21 09:18:17 -0500

    Add bash quicklaunch script

---
+++ yahboom_rosmaster_bringup/scripts/rosmaster_x3_gazebo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Single script to launch the Yahboom ROSMASTERX3 with Gazebo and ROS 2 Controllers
+
+cleanup() {
+    echo "Cleaning up..."
+    echo "Restarting ROS 2 daemon..."
+    ros2 daemon stop
+    sleep 1
+    ros2 daemon start
+    echo "Terminating all ROS 2-related processes..."
+    kill 0
+    exit
+}
+
+# Set up cleanup trap
+trap 'cleanup' SIGINT
+
+echo "Launching Gazebo simulation..."
+ros2 launch yahboom_rosmaster_gazebo yahboom_rosmaster.gazebo.launch.py \
+    load_controllers:=true \
+    world_file:=empty.world \
+    use_rviz:=true \
+    use_robot_state_pub:=true \
+    use_sim_time:=true \
+    x:=0.0 \
+    y:=0.0 \
+    z:=0.05 \
+    roll:=0.0 \
+    pitch:=0.0 \
+    yaw:=0.0
