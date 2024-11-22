480976af4692856b8519eedeaa4397d236a94d88
Automatic Addison <automaticaddison@gmail.com>
2024-11-21 22:44:26 -0500

    Set default to empty.world

--- yahboom_rosmaster_bringup/scripts/rosmaster_x3_gazebo.sh
+++ yahboom_rosmaster_bringup/scripts/rosmaster_x3_gazebo.sh
@@ -18,7 +18,7 @@ trap 'cleanup' SIGINT
 echo "Launching Gazebo simulation..."
 ros2 launch yahboom_rosmaster_gazebo yahboom_rosmaster.gazebo.launch.py \
     load_controllers:=true \
-    world_file:=house.world \
+    world_file:=empty.world \
     use_rviz:=true \
     use_robot_state_pub:=true \
     use_sim_time:=true \
