27326988d315cbd94824341436a81c4e0eb57a08
Automatic Addison <automaticaddison@gmail.com>
2024-12-06 09:08:00 -0500

    Added better cleanup logic for ros2 navigation

--- yahboom_rosmaster_bringup/scripts/rosmaster_x3_navigation.sh
+++ yahboom_rosmaster_bringup/scripts/rosmaster_x3_navigation.sh
@@ -2,14 +2,31 @@
 # Single script to launch the Yahboom ROSMASTERX3 with Gazebo, Nav2 and ROS 2 Controllers
 
 cleanup() {
-   echo "Cleaning up..."
-   echo "Restarting ROS 2 daemon..."
-   ros2 daemon stop
-   sleep 1
-   ros2 daemon start
-   echo "Terminating all ROS 2-related processes..."
-   kill 0
-   exit
+    echo "Cleaning up..."
+    echo "Killing all ROS 2 and Gazebo related processes..."
+    sleep 5
+    pkill -9 -f "ros2|gazebo|gz|nav2|amcl|bt_navigator|nav_to_pose|rviz2|python3|assisted_teleop|cmd_vel_relay|robot_state_publisher|joint_state_publisher|move_to_free|mqtt|autodock|cliff_detection|moveit|move_group|basic_navigator"
+    sleep 0.2
+
+    echo "Removing IPC resources..."
+    ipcrm -a
+    sleep 0.2
+
+    echo "Cleaning up shared memory..."
+    rm -rf /dev/shm/fastrtps_* /dev/shm/sem.fastrtps_*
+    sleep 0.2
+
+    echo "Cleaning up temporary files..."
+    rm -rf /tmp/fastrtps* /tmp/ros* /tmp/snap.ros* /tmp/*.sem /var/run/ros*
+    sleep 0.2
+
+    echo "Restarting ROS 2 daemon..."
+    ros2 daemon stop
+    sleep 0.2
+    ros2 daemon start
+
+    kill 0
+    exit
 }
 
 # Set up cleanup trap
@@ -28,24 +45,25 @@ fi
 
 echo "Launching Gazebo simulation with Nav2..."
 ros2 launch yahboom_rosmaster_bringup rosmaster_x3_navigation_launch.py \
-   enable_odom_tf:=false \
-   headless:=False \
-   load_controllers:=true \
-   world_file:=cafe.world \
-   use_rviz:=true \
-   use_robot_state_pub:=true \
-   use_sim_time:=true \
-   x:=0.0 \
-   y:=0.0 \
-   z:=0.20 \
-   roll:=0.0 \
-   pitch:=0.0 \
-   yaw:=0.0 \
-   "$SLAM_ARG" \
-   map:=/home/ubuntu/ros2_ws/src/yahboom_rosmaster/yahboom_rosmaster_navigation/maps/cafe_world_map.yaml &
+    enable_odom_tf:=false \
+    headless:=False \
+    load_controllers:=true \
+    world_file:=cafe.world \
+    use_rviz:=true \
+    use_robot_state_pub:=true \
+    use_sim_time:=true \
+    x:=0.0 \
+    y:=0.0 \
+    z:=0.20 \
+    roll:=0.0 \
+    pitch:=0.0 \
+    yaw:=0.0 \
+    "$SLAM_ARG" \
+    map:=/home/ubuntu/ros2_ws/src/yahboom_rosmaster/yahboom_rosmaster_navigation/maps/cafe_world_map.yaml &
 
 echo "Waiting 25 seconds for simulation to initialize..."
 sleep 25
+
 echo "Adjusting camera position..."
 gz service -s /gui/move_to/pose --reqtype gz.msgs.GUICamera --reptype gz.msgs.Boolean --timeout 2000 --req "pose: {position: {x: 0.0, y: -2.0, z: 2.0} orientation: {x: -0.2706, y: 0.2706, z: 0.6533, w: 0.6533}}"
 
